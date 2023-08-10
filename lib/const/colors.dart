ee550a98225abc3da68ab0f4a0e622493fc3f63a
Moniruddin Laskar <mrlaskar70@gmail.com>
2023-08-10 10:54:16 +0530

    basic folder structure done

---
+++ lib/const/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+Color black = const Color(0xFF000000);
+
+Color grey = const Color(0xFF9E9E9E);
+
+Color darkgreen = const Color(0xFF046B3E);
+
+Color lightgreen = const Color(0xFF87C558);
+
+Color white = const Color(0xFFFFFFFF);
+
+Color red = Colors.red;
+
+Color blue = const Color(0xFF2196F3);
